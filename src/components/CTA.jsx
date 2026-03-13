0f8fd691db9b26724b11207d8f289ff7956188f5
ariqazmain <ariqazmain12@gmail.com>
2026-03-13 22:08:35 +0600

    add pdd

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,6 +1,6 @@
 const CTA = ({ text, href }) => {
   return (
-    <div className="cta-container overflow-visible">
+    <div className="cta-container overflow-visible !px-[40px] !py-[10px]">
       <span>
         <a href={href} className="cta-btn">
           {text}
